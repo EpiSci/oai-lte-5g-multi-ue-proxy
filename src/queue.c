1a8c6aef346fc700adfbcba3a65d50e345af2f2c
Melissa Elkadi <melissa@episci.com>
2022-03-22 06:55:44 -0700

    Adding SPDX License Identifier in each source file

--- src/queue.c
+++ src/queue.c
@@ -1,3 +1,23 @@
+#/*
+# * Licensed to the EPYSYS SCIENCE (EpiSci) under one or more
+# * contributor license agreements.
+# * The EPYSYS SCIENCE (EpiSci) licenses this file to You under
+# * the Episys Science (EpiSci) Public License (Version 1.1) (the "License"); you may not use this file
+# * except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      https://github.com/EpiSci/oai-lte-5g-multi-ue-proxy/blob/master/LICENSE
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *-------------------------------------------------------------------------------
+# * For more information about EPYSYS SCIENCE (EpiSci):
+# *      bo.ryu@episci.com
+# */
+
 #include "queue.h"
 #include <string.h>
 #include <assert.h>
